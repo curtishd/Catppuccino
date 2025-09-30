b2bad7c6cd8cf805383ad15f5b1412bb1b64e28c
curtishd <1023632660@qq.com>
2025-09-30 09:22:29 +0800

    perf: Use WeakReference.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,6 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
 }
--- src/main/kotlin/conf/ResourceLoader.kt
+++ src/main/kotlin/conf/ResourceLoader.kt
@@ -3,20 +3,24 @@ package me.cdh.conf
 import me.cdh.Animate
 import me.cdh.Random
 import java.awt.image.BufferedImage
+import java.lang.ref.WeakReference
 import javax.imageio.ImageIO
 
 object ResourceLoader {
-    var catType = listOf(
-        "calico_cat",
-        "grey_tabby_cat",
-        "orange_cat",
-        "white_cat",
+    var catType = WeakReference(
+        listOf(
+            "calico_cat",
+            "grey_tabby_cat",
+            "orange_cat",
+            "white_cat",
+        )
     )
-    private val selectedCatType = catType[Random.nextInt(0, catType.size)]
+    private val selectedCatType: WeakReference<String> =
+        WeakReference(catType.get()!![Random.nextInt(0, catType.get()!!.size)])
 
     fun loadFrames(actionName: String, frameCount: Int): List<BufferedImage> = (1..frameCount).mapNotNull { frameNum ->
         javaClass.classLoader.getResourceAsStream(
-            "$selectedCatType/${actionName.lowercase()}/${actionName.lowercase()}_$frameNum.png"
+            "${selectedCatType.get()}/${actionName.lowercase()}/${actionName.lowercase()}_$frameNum.png"
         )?.use { ImageIO.read(it) }
     }
 
